eebbfce28a962a5d1aaff01e8ba229ce212c6f8a
TheGeodez <TheGeodez@yandex.ru>
2018-11-12 14:26:27 +0300

    job app made

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'ab030830',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
